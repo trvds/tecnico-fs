11b62625c197b0ea08ff2b548470e4f22aaf9b2b
trvds <tiagovs02@gmail.com>
2021-12-26 05:38:44 +0000

    Corrected Makefile and fixed copy_to_external_simple

--- tecnicofs/tests/copy_to_external_simple.c
+++ tecnicofs/tests/copy_to_external_simple.c
@@ -27,7 +27,7 @@ int main() {
 
     assert(fread(to_read, sizeof(char), strlen(str), fp) == strlen(str));
     
-    assert(strcmp(str, to_read) == 0);
+    assert(strncmp(str, to_read, strlen(str)) == 0);
 
     assert(fclose(fp) != -1);
 
