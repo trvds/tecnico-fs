82ab67fee7a3f18f90b763924068b5b2592b5ccf
trvds <tiagovs02@gmail.com>
2022-01-14 22:27:46 +0000

    Fixed multithread_test2.c

--- tecnicofs/tests/multithread_test2.c
+++ tecnicofs/tests/multithread_test2.c
@@ -2,8 +2,9 @@
 #include <assert.h>
 #include <string.h>
 
-#define NUMBER_OF_THREADS 20 // Não pode ser mais que 20 pela limitação do file system de ter 20 ficheiros abertos consecutivamente
-
+#define NUMBER_OF_THREADS 100
+#define SIZE 12
+#define N 4
 /**
    This test checks if the creation, writing and reading is working
    with multiple threads.
@@ -11,22 +12,23 @@
 
 
 struct arguments{
-    char *path;
-    char *input;
+    char path[N];
+    char input[SIZE];
 };
 
 void *fnWrite(void *arg){
     /* Buffers for the path and input */
     struct arguments *args = (struct arguments*) arg;
-    char *path;
-    char *input;
-    memcpy(path, args->path, strlen(path));
-    memcpy(input, args->input, strlen(input));
-    /* Create file*/
+    
+    char path[SIZE];
+    memcpy(path, args->path, N);
+    char input[SIZE];
+    memcpy(input, args->input, SIZE);
+    /* Open File to append */
     int fd = tfs_open(path, TFS_O_APPEND);
     assert(fd != -1);
 
-    assert(tfs_write(fd, input, strlen(input)) == strlen(input));
+    assert(tfs_write(fd, input, SIZE) == SIZE);
     assert(tfs_close(fd) != -1);
     return NULL;
 }
@@ -34,14 +36,17 @@ void *fnWrite(void *arg){
 int main() {
     pthread_t tid[NUMBER_OF_THREADS];
     struct arguments args;
-    char *input = 'Lorem Ipsum';
-    /* Declaring the arguments in the argument structure */
+
+    char *input = "Lorem Ipsum";
+    memcpy(args.input, input, SIZE);
     char *path = "/f1";
-    char output[strlen(input)];
-    memcpy(args.path, path, strlen(path));
-    memset(args.input, input, strlen(input));
+    memcpy(args.path, path, N);
 
     assert(tfs_init() != -1);
+
+    int f = tfs_open(path, TFS_O_CREAT);
+    assert(f != -1);
+    assert(tfs_close(f) != -1);
 
     for (int i = 0; i < NUMBER_OF_THREADS; i++){
         if (pthread_create(&tid[i], NULL, fnWrite, (void*)&args) != 0)
@@ -51,14 +56,15 @@ int main() {
     for (int i = 0; i < NUMBER_OF_THREADS; i++){
         pthread_join(tid[i], NULL);
     }
-    
+
+    char output[SIZE];
+
     int fd = tfs_open(path, 0);
     assert(fd != -1);
 
     for (int i = 0; i < NUMBER_OF_THREADS; i++){
-        assert(tfs_read(fd, output, strlen(output) - 1) == strlen(input));
-        output[strlen(input)] == '\0';
-        assert(strcmp(output, args.input) == 0);
+        assert(tfs_read(fd, output, SIZE) == SIZE);
+        assert(strcmp(output, input) == 0);
     }
 
     printf("Sucessful test\n");
