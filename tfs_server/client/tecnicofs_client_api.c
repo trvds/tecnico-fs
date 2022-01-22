9ce97b257fc7da00e1a5c4eba53811c0f709b8a1
trvds <tiagovs02@gmail.com>
2022-01-22 17:16:12 +0000

    Sync github to work on desktop.

--- tfs_server/client/tecnicofs_client_api.c
+++ tfs_server/client/tecnicofs_client_api.c
@@ -1,13 +1,36 @@
 #include "tecnicofs_client_api.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1000
+
+static int fserver, fclient;
+static char buffer[BUFFER_SIZE];
+static char* client_path;
+static char* server_path;
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
-    /* TODO: Implement this */
-    return -1;
+    client_path = client_pipe_path;
+    server_path = server_pipe_path;
+    unlink(client_pipe_path);
+    if (mkfifo (client_pipe_path, 0777) < 0) return -1;
+
+    if ((fserver = open (client_pipe_path, O_WRONLY)) < 0) return -1;
+    if ((fclient = open (server_pipe_path, O_RDONLY)) < 0) return -1;
+    return 0;
 }
 
 int tfs_unmount() {
-    /* TODO: Implement this */
-    return -1;
+    strcpy(buffer, "OP_CODE=2 | ")
+    unlink(client_path);
+    close (fserver);
+    close (fclient);
+    return 0;
 }
 
 int tfs_open(char const *name, int flags) {
--- tfs_server/fs/tfs_server.c
+++ tfs_server/fs/tfs_server.c
@@ -1,6 +1,65 @@
 #include "operations.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define S 1
+#define BUFFER_SIZE 1000
+
+
+
+
+void requestHandler(char buffer[]){
+    char * opcode = strtok(buffer, "|");
+
+    if (strcmp(opcode, "OP_CODE=1") == 0){
+        char client_pipe_name[40] = strtok(NULL, "|");
+
+    }
+    else if (strcmp(opcode, "OP_CODE=2") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+
+    }
+    else if (strcmp(opcode, "OP_CODE=3") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+        char name[40] = strtok(NULL, "|");
+        int flags = strtok(NULL, "|") - "0";
+
+    }
+    else if (strcmp(opcode, "OP_CODE=4") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+        int fhandle = strtok(NULL, "|") - "0";
+
+    }
+    else if (strcmp(opcode, "OP_CODE=5") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+        int fhandle = strtok(NULL, "|") - "0";
+        size_t len = (size_t) (strtok(NULL, "|") - "0");
+        char buffer_content[len] = strtok(NULL, "|");
+    }
+    else if (strcmp(opcode, "OP_CODE=6") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+        int fhandle = strtok(NULL, "|") - "0";
+        size_t len = (size_t) (strtok(NULL, "|") - "0");
+
+    }
+    else if (strcmp(opcode, "OP_CODE=7") == 0){
+        int session_id = strtok(NULL, "|") - "0";
+
+    }
+
+    
+}
+
+
 
 int main(int argc, char **argv) {
+    pthread_t tid[S];
+    int thread_counter = 0;
 
     if (argc < 2) {
         printf("Please specify the pathname of the server's pipe.\n");
@@ -8,9 +67,24 @@ int main(int argc, char **argv) {
     }
 
     char *pipename = argv[1];
+    int fserver, buffer_reader;
+    char buffer[BUFFER_SIZE];
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-    /* TO DO */
+    unlink(pipename);
+    
+    if (mkfifo (pipename, 0777) < 0) exit (EXIT_FAILURE);
+
+    if ((fserver = open (pipename, O_RDONLY)) < 0) exit(EXIT_FAILURE);
+
+    while(1){
+        buffer_reader = read(fserver, buffer, BUFFER_SIZE);
+        requestHandler(buffer);
+
+    }
+
+    close (fserver);
+    unlink(pipename);
 
     return 0;
 }
